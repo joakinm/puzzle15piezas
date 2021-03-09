ae48405f3fe123cb1683c8b0fe15e5d58ca83a2f
Joaquin Martinez <joaquinivanmartinez@gmail.com>
2021-03-09 16:56:11 +0100

    mejorando codigo

--- puzzle.js
+++ puzzle.js
@@ -1,97 +1,80 @@
-var arreglo = new Array();
-function moverpieza()
-{
-    for (i=0;i<arreglo.length;i++)
-    {
-        document.getElementById("bt"+i).addEventListener("onclick",moverpieza());
+let arreglo = new Array();
+function moverpieza() {
+    for (i = 0; i < arreglo.length; i++) {
+        document.getElementById('bt'+i).addEventListener('onclick', moverpieza());
     }
 }
 
-function crearpuzzle()
-{
-    var repite = false;
-    var i;
-    var j;
-    var nrorandom = (Math.floor(Math.random() * 15))+1;
-    for (j=0;j<15;j++)
-    {
-        while(repite == false && i<=arreglo.length)
-        {
-            if (arreglo[i] == nrorandom)
-            {
-                nrorandom = (Math.floor(Math.random() * 15))+1;
-                i=0;    
+function crearpuzzle() {
+    let posicion;
+    let contador;
+    let nrorandom = (Math.floor(Math.random() * 15)) + 1;
+    for (contador = 0; contador < 15; contador++) {
+        while (posicion <= arreglo.length) {
+            if (arreglo[posicion] == nrorandom) {
+                nrorandom = (Math.floor(Math.random() * 15)) + 1;
+                posicion = 0;    
             }
-            else
-            {
-                i++;
+            else {
+                posicion++;
             }
         }
-     arreglo.push( nrorandom);
-     i = 0;
+     arreglo.push(nrorandom);
+     posicion = 0;
     }
-    arreglo.push("");
+    arreglo.push('');
 }
 
-function armarpuzzle ()
-{
+function armarpuzzle() {
     crearpuzzle();
     refrescarpuzzle();
 }
 
-function gano () 
-{
-    var i;
-    var numerosbien;
-    numerosbien = 0;
-    i=0;
-    while(numerosbien != 15 && i<=arreglo.length)
-    {
-        if (arreglo[i] == i+1)
-        {
+function gano() {
+    let i = 0;
+    let numerosbien = 0;
+    while(numerosbien != 15 && i <= arreglo.length) {
+        if (arreglo[i] == i + 1) {
             numerosbien++;
             i++;
         }
-        else
-        {
+        else {
             i++;
         }
     }
-    if (numerosbien == 15)
-    {
+    if (numerosbien == 15) {
         alert('GANOOOOO!!!');
     }
 }
 
-function refrescarpuzzle()
-{
-    var i;
-    gano ();
-    document.getElementById("juego").innerHTML = "";
-    for (i=0;i<arreglo.length;i++)
-    {
-        var Fichas = document.createElement('button');
-        var texto = document.createTextNode(arreglo[i]);
-        var divTexto = document.createElement('div');
-        divTexto.setAttribute('class','juego');
-        Fichas.appendChild(texto);
-        Fichas.setAttribute("value",texto);
-        Fichas.setAttribute("class","BtJuego");
-        Fichas.setAttribute("name",i);
-        Fichas.onclick = function(i)
-        {
-            var j= (arreglo.findIndex((element) => element == ""));
-            var movimiento = event.currentTarget.name;
-            if ((movimiento == (j+1) ) || (movimiento ==(j-1) ) || (movimiento == (j+4) ) || (movimiento == (j-4) ))
-            {
-            var valor = arreglo[movimiento];
+function refrescarpuzzle() {
+    let contador;
+    gano();
+    document.getElementById('juego').innerHTML = '';
+    for (contador = 0; contador < arreglo.length; contador++) {
+        let fichas = document.createElement('button');
+        const texto = document.createTextNode(arreglo[contador]);
+        let divTexto = document.createElement('div');
+        divTexto.setAttribute('class', 'juego');
+        fichas.appendChild(texto);
+        fichas.setAttribute('value', texto);
+        fichas.setAttribute('class', 'BtJuego');
+        fichas.setAttribute('name', contador);
+
+        fichas.onclick = (i) => {
+            const j = (arreglo.findIndex((element) => element == ''));
+            const movimiento = event.currentTarget.name;
+            if ((movimiento == (j + 1) ) || (movimiento == (j - 1) ) || (movimiento == (j + 4) ) || (movimiento == (j - 4) )) {
+            const valor = arreglo[movimiento];
             arreglo[event.currentTarget.name] = arreglo[j];
             arreglo[j] = valor;
-            refrescarpuzzle ();
+            refrescarpuzzle();
             }
-            else{alert('no se puede hacer ese movimiento')}
+            else {
+                alert('no se puede hacer ese movimiento');
+            }
         };
-        divTexto.appendChild(Fichas);
-        document.getElementById("juego").appendChild(divTexto);
+        divTexto.appendChild(fichas);
+        document.getElementById('juego').appendChild(divTexto);
     }
 }
